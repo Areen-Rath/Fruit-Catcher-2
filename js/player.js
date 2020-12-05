d90972715aa009cb61f4e21bf9fcbdf60c060e64
Areen-Rath <70901420+Areen-Rath@users.noreply.github.com>
2020-12-05 21:20:49 +0530

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,50 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    updatePosAndDis(){
+        database.ref('players/player1').update({
+            score: 0,
+            distance: 0
+        })
+
+        database.ref('players/player2').update({
+            score: 0,
+            distance: 0
+        })
+    }
+    
+}
